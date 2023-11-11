0199ac2840e37352dda95a2d3d88eff32a738005
DK Kim <dkim693@wisc.edu>
2023-11-11 16:20:06 -0600

    faxed navbar and added image logo

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,37 +1,35 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.css";
 import "react-router-dom";
 
 export default function ChessNavbar() {
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
-      <Container>
-        {/* Logo and Home Link */}
-        <Navbar.Brand href="/">
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <img
+            width="35vh"
+            style={{ marginRight: "5px", marginLeft: "5px" }}
+            src="https://github.com/DK-Kim4312/images/blob/main/chess_image.png?raw=true"
             alt="Chess Logo"
-            src="path_to_your_logo.png"  
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          Beat a Grandmaster
-        </Navbar.Brand>
+          />
+          <Navbar.Brand href="/">Beat a GM</Navbar.Brand>
+        </div>
+        <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/play">Play</Nav.Link>
 
-        {/* Navbar Toggle for Responsive Design */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        {/* Navbar Links */}
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            {/* Play Button */}
-            <Nav.Link href="/play">Play</Nav.Link>
-
-            {/* News Button */}
-            <Nav.Link href="/news">News</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+              {/* News Button */}
+              <Nav.Link href="/news">News</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
